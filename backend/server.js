0410d498c3d27482a2aede9046587a4a843104d1
CalmCoder2357 <140947114+CalmCoder2357@users.noreply.github.com>
2026-03-03 01:30:53 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
-
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 dotenv.config();
 connectDB();
 
